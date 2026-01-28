b990079ea7253adb8138218dd4353675f0505b0c
lily <lilymason@msn.com>
2026-01-28 15:42:41 -0800

    Faculty can upload documents for student

--- obj/Debug/net9.0/Database.AssemblyInfo.cs
+++ obj/Debug/net9.0/Database.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eab5b308daf8902a38cb48e6064d99028920410b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+506bfbb4ad2593a63439907ff1294eb5b1badee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
